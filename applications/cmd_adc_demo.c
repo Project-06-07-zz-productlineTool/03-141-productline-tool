c94806c1620492d212479a77674b707274459782
Gaogao <91398548+vangao-gg@users.noreply.github.com>
2024-05-28 13:29:43 +0800

    [acheieve][adc check][6 channel and vaule sheet]

--- applications/cmd_adc_demo.c
+++ applications/cmd_adc_demo.c
@@ -19,16 +19,45 @@
 #include <rtdevice.h>
 
 #define ADC_DEV_NAME        "adc1"      /* ADC 设备名称 */
-// #define ADC_DEV_CHANNEL     5           /* ADC 通道 */
-#define REFER_VOLTAGE       330         /* 参考电压 3.3V,数据精度乘以100保留2位小数*/
+#define REFER_VOLTAGE       3300         /* 参考电压 3.3V,数据精度乘以100保留2位小数*/
 #define CONVERT_BITS        (1 << 12)   /* 转换位数为12位 */
+#define CHANNEL_NUM 6
 
-static int adc_vol_demo(int argc, char *argv[])
+static rt_uint32_t true_value_sheet[CHANNEL_NUM] = {2750,1650,2200,2750,1650,1650};
+static rt_uint32_t err_range = 100;
+
+rt_uint32_t adc_vol[CHANNEL_NUM] = {0};
+rt_adc_device_t adc_dev;
+
+void match_and_report(void) {
+    
+    rt_err_t ret = RT_EOK;
+    rt_uint32_t value;
+
+    for (rt_uint8_t i = 0; i < CHANNEL_NUM;i++)
+    {
+        ret = rt_adc_enable(adc_dev, i);
+        value = rt_adc_read(adc_dev, i);
+        adc_vol[i] = value * REFER_VOLTAGE / CONVERT_BITS;
+        ret = rt_adc_disable(adc_dev, i);
+
+        if(abs(true_value_sheet[i] - adc_vol[i]) <  err_range)
+        {
+            rt_kprintf("channel %d check pass, value is %d mv\r\n",i,adc_vol[i]);
+        }
+        else 
+        {
+            rt_kprintf("channel %d check error, value is %d mv\r\n",i,adc_vol[i]);
+        }
+        
+    }
+}
+
+static int adc_check(int argc, char *argv[])
 {
-    rt_adc_device_t adc_dev;
+ 
     rt_uint32_t value, vol;
     rt_err_t ret = RT_EOK;
-    rt_uint8_t channel_sel = 0;
 
     /* 查找设备 */
     adc_dev = (rt_adc_device_t)rt_device_find(ADC_DEV_NAME);
@@ -37,32 +66,10 @@ static int adc_vol_demo(int argc, char *argv[])
         rt_kprintf("adc sample run failed! can't find %s device!\n", ADC_DEV_NAME);
         return RT_ERROR;
     }
-    if(argc == 2 )
-    {
-        channel_sel = atoi(argv[1]);
-    }
-    else
-    {
-        rt_kprintf("please enter adc channel\r\n");
-        return ret;
-    }
 
-
-    /* 使能设备 */
-    ret = rt_adc_enable(adc_dev, channel_sel);
-
-    /* 读取采样值 */
-    value = rt_adc_read(adc_dev, channel_sel);
-    rt_kprintf("the value is :%d \n", value);
-
-    /* 转换为对应电压值 */
-    vol = value * REFER_VOLTAGE / CONVERT_BITS;
-    rt_kprintf("the voltage is :%d.%02d \n", vol / 100, vol % 100);
-
-    /* 关闭通道 */
-    ret = rt_adc_disable(adc_dev, channel_sel);
+    match_and_report();
 
     return ret;
 }
 /* 导出到 msh 命令列表中 */
-MSH_CMD_EXPORT(adc_vol_demo, adc voltage convert sample);
+MSH_CMD_EXPORT(adc_check, adc voltage convert sample);
